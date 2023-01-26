452d20d05ff65edd4670931338527c672cc7eb28
Abid Hussain <chabidhussain.132@gmail.com>
2023-01-26 18:42:44 +0500

    complete launchpad list section

--- src/component/Launchpad_card/Launchpad_card.jsx
+++ src/component/Launchpad_card/Launchpad_card.jsx
@@ -3,11 +3,46 @@ import React from "react";
 import "./Launchpad_card.css";
 import LockIcon from "@mui/icons-material/Lock";
 import { Link } from "react-router-dom";
+import Countdown from "react-countdown";
 {
   /* {LockIcon}  */
 }
 
 function Launchpad_card(props) {
+  console.log("props", props);
+  const Completionist = () => (
+    <strong>
+      <span>00</span>
+      <span className="dotdot">:</span>
+      <span>00</span>
+      <span className="dotdot">:</span>
+      <span>00</span>
+      <span className="dotdot">:</span>
+      <span>00</span>
+    </strong>
+  );
+
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    // setTimeEnded(completed)
+
+    if (completed) {
+      // Render a completed state
+      // setUnlockDisable(false);
+      return <Completionist />;
+    } else {
+      return (
+        <strong>
+          <span>{days}</span>
+          <span className="dotdot">:</span>
+          <span>{hours}</span>
+          <span className="dotdot">:</span>
+          <span>{minutes}</span>
+          <span className="dotdot">:</span>
+          <span>{seconds}</span>
+        </strong>
+      );
+    }
+  };
   return (
     <>
       <div className="container">
@@ -16,64 +51,85 @@ function Launchpad_card(props) {
             <div className="card-body bg_color ">
               <div className=" d-flex justify-content-between align-items-center ">
                 <img src={props.img_card} alt="" className="img_card" />
+                {console.log("color", props.timeInfo.color)}
                 <div>
-                  <p className=" p_bg text-white px-2 py-1">
+                  <p
+                    className={` px-2 rounded-pill `}
+                    style={{
+                      backgroundColor: `${props.timeInfo.backgroundcolor}`,
+                    }}
+                  >
                     <svg
-                      stroke="currentColor"
-                      className="icon_lock"
-                      fill="currentColor"
-                      stroke-width="0"
-                      viewBox="0 0 1024 1024"
-                      height="16"
-                      width="16"
+                      className=""
                       xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      version="1.1"
+                      style={{ marginRight: "5px" }}
                     >
-                      <path d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 1 0-56 0z"></path>
+                      <circle
+                        cx="5"
+                        cy="5"
+                        r="5"
+                        fill={`${props.timeInfo.color}`}
+                      />
                     </svg>
-                    upcoming
+                    <span
+                      className="text-"
+                      style={{
+                        color: `${props.timeInfo.color}`,
+                      }}
+                    >
+                      <small>{props.timeInfo.title}</small>
+                    </span>
                   </p>
                 </div>
               </div>
 
               <div className="my-3">
-                <p className="text-white mb-1 hh"> {props.para_1}</p>
-                <p className="text-white mb-1 kk">{props.para_2}</p>
+                <p className="text-white mb-1 hh"> {props.tokenName}</p>
+                <p className="text-white mb-1 kk">{props.totalSupply}</p>
               </div>
-              <p className="text-white progress_text mb-2">
-                Progress{props.para_3}
-              </p>
+              <p className="text-white progress_text mb-2">ICO Progress(%)</p>
               <div className="progress">
                 <div
                   className="progress-bar"
                   role="progressbar"
                   aria-valuenow="50"
                   aria-valuemin="50"
-                  style={{width:"20%"}}
+                  style={{ width: `${props.progressInPercent}%` }}
                   aria-valuemax="100"
                 ></div>
               </div>
               <div className="d-flex justify-content-between mt-2">
-                <p className="text-white progress_text">{props.BNB_1}</p>
-                <p className=" text-white progress_text"> {props.BNB_2}</p>
+                <p className="text-white progress_text">
+                  {props.soldAmountForProgress}
+                </p>
+                <p className=" text-white progress_text">
+                  {" "}
+                  {props.totalSupplyForProgress}
+                </p>
               </div>
               <hr className="hr" />
 
               <div className="d-flex justify-content-between text-white">
                 <p>
-                  Sale Starts In: <br />
-                  <strong>
-                    <span>00</span>
-                    <span className="dotdot">:</span>
-                    <span>00</span>
-                    <span className="dotdot">:</span>
-                    <span>06</span>
-                    <span className="dotdot">:</span>
-                    <span>41</span>
-                  </strong>{" "}
+                  <strong> {props.timeInfo.timerTitle}</strong> <br />
+                  <Countdown
+                    date={
+                      Date.now() +
+                      (String(props.timeInfo.seconds * 1000) - Date.now())
+                    }
+                    renderer={renderer}
+                  />
                 </p>
 
                 <div>
-                  <Link to="/Launchpad_list_view" className="btn max_btn_color "  role="button">
+                  <Link
+                    to="/Launchpad_list_view"
+                    className="btn max_btn_color "
+                    role="button"
+                  >
                     View
                   </Link>{" "}
                 </div>
--- src/component/Launchpad_list/Launchpad_list.jsx
+++ src/component/Launchpad_list/Launchpad_list.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./Launchpad_list.css";
 import Tabs from "@mui/material/Tabs";
@@ -11,6 +11,17 @@ import Card_img2 from "../Assets/unity.png";
 import Card_img3 from "../Assets/pepsi.png";
 import Card_img4 from "../Assets/pinkswap.a95de4f3.png";
 import Form from "react-bootstrap/Form";
+import {
+  pinkSaleLockContract,
+  pinkSaleLockAbi,
+  tokenAbi,
+  tokenAdress,
+  PinkSaleICOFactoryContractAddress,
+  PinkSaleICOFactoryContractABI,
+  PinksaleICOContractABI,
+  PinksaleICOContractAddress,
+} from "../../utilies/Contract";
+import { loadWeb3 } from "../../connectivity/connectivity";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -47,10 +58,85 @@ function a11yProps(index) {
 
 export default function BasicTabs() {
   const [value, setValue] = React.useState(0);
+  const [totalIcos, setTotalIcos] = useState([]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const allIcos = async () => {
+    const web3 = window.web3;
+    let acc = await loadWeb3();
+    let allIcosInfo = [];
+    let obj;
+    let pinkSaleFactoryICO = new web3.eth.Contract(
+      PinkSaleICOFactoryContractABI,
+      PinkSaleICOFactoryContractAddress
+    );
+    let totalIcos = await pinkSaleFactoryICO.methods.totalICOs().call();
+    console.log("icos", totalIcos);
+    for (let index = 0; index < totalIcos.length; index++) {
+      const contractAddress = totalIcos[index];
+      let pinkSaleICO = new web3.eth.Contract(
+        PinksaleICOContractABI,
+        contractAddress
+      );
+      let tokenDetail = await pinkSaleICO.methods.tokeninfo().call();
+      let icoInfo = await pinkSaleICO.methods.ICO_info().call();
+      let icoProgress = await pinkSaleICO.methods.status().call();
+      let soldPercent = (icoProgress.sold_amount / icoInfo.token_supply) * 100;
+      // console.log("soldPercent", soldPercent);
+      let currentDateSeconds = Math.round(new Date().getTime() / 1000);
+      console.log(currentDateSeconds);
+      let endTimeLessthenStartTime;
+      let timeInfo;
+
+      if (currentDateSeconds < Number(icoInfo.ICO_start)) {
+        timeInfo = {
+          timerTitle: "  ICO Starts In",
+          seconds: icoInfo.ICO_start,
+          backgroundcolor: "#fdfaea",
+          color: "#d29813",
+          title: "Upcoming",
+        };
+      } else if (currentDateSeconds < Number(icoInfo.ICO_end)) {
+        timeInfo = {
+          timerTitle: "  ICO Ends In",
+          seconds: icoInfo.ICO_end,
+          backgroundcolor: "#d1fae5",
+          color: "#10b981",
+          title: "ICO Live",
+        };
+      } else {
+        timeInfo = {
+          timerTitle: "  ICO Ended ",
+          seconds: icoInfo.ICO_end,
+          backgroundcolor: "#ffeaef",
+          color: "#ff3465",
+          title: "ICO Ended",
+        };
+      }
+      obj = {
+        tokenName: tokenDetail.name,
+        tokenSymbol: tokenDetail.symbol,
+        totalSupply: web3.utils.fromWei(icoInfo.token_supply),
+        soldAmount: web3.utils.fromWei(icoProgress.sold_amount),
+        startTime: icoInfo.ICO_start,
+        endTime: icoInfo.ICO_end,
+        progressInPercent: soldPercent,
+        timeInfo: timeInfo,
+      };
+      // allIcosInfo.push(obj);
+      allIcosInfo = [{ ...obj }, ...allIcosInfo];
+      // console.log("icoInfo", icoInfo.token_supply);
+      // console.log("icoProgress", icoProgress);
+    }
+    // console.log("objectobject", allIcosInfo);
+
+    setTotalIcos([...allIcosInfo]);
+  };
+  useEffect(() => {
+    allIcos();
+  }, []);
 
   return (
     <Box sx={{ width: "100%" }}>
@@ -71,61 +157,30 @@ export default function BasicTabs() {
           <Form.Control
             type="url"
             className="url_input input_flied_of_pink "
-            placeholder="Enter token name and token symbal"
+            placeholder="Enter token address"
             autoComplete="on"
           />
         </div>
       </Box>
       <TabPanel value={value} index={0} className="">
         <div className="launh_grid">
-          <Launchpad_card
-            img_card={Card_img}
-            para_1="Tweetfi"
-            para_2="1 BNB = 30,000 TW."
-            para_3="(0.00%)"
-            BNB_1="0 BNB"
-            BNB_2="200 BNB"
-          />
-          <Launchpad_card
-            img_card={Card_img2}
-            para_1="untitled presale"
-            para_2="150 BNB - 250 BNB"
-            para_3="(0.00%)"
-            BNB_1="0 BNB"
-            BNB_2="250 BNB"
-          />
-          <Launchpad_card
-            img_card={Card_img3}
-            para_1="-"
-            para_2="0.1 BNB"
-            para_3="(0.00%)"
-            BNB_1="0 BNB"
-            BNB_2="200 BNB"
-          />
-          <Launchpad_card
-            img_card={Card_img4}
-            para_1="untitled presale"
-            para_2="1 USDC - 2 USDC"
-            para_3="(0.00%)"
-            BNB_1="0 USDC"
-            BNB_2="2 USDC"
-          />
-          <Launchpad_card
-            img_card={Card_img4}
-            para_1="aa"
-            para_2="0.002 BNB - 0.003 BNB"
-            para_3="(0.00%)"
-            BNB_1="0 BNB"
-            BNB_2="200 BNB"
-          />
-          <Launchpad_card
-            img_card={Card_img4}
-            para_1="Etherst"
-            para_2="120 BNB"
-            para_3="(0.00%)"
-            BNB_1="0 BNB"
-            BNB_2="200 BNB"
-          />
+          {totalIcos.map((item, index, arr) => {
+            return (
+              <Launchpad_card
+                img_card={Card_img}
+                totalSupply={`1 BNB = ${item.totalSupply} ${item.tokenSymbol}`}
+                para_3="(0.00%)"
+                soldAmountForProgress={`${item.soldAmount} ${item.tokenSymbol}`}
+                totalSupplyForProgress={`${item.totalSupply} ${item.tokenSymbol}`}
+                tokenName={item.tokenName}
+                tokenSymbol={item.tokenSymbol}
+                startTime={item.startTime}
+                endTime={item.endTime}
+                progressInPercent={item.progressInPercent}
+                timeInfo={item.timeInfo}
+              />
+            );
+          })}
         </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
